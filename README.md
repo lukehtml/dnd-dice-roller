0a6efb9c520ce5352726150259c8d0f1ba7287db
lukehtml <kedoooooooooooo@gmail.com>
2026-01-24 09:19:19 +0100

    added alot more dice

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # dnd-dice-roller
-the name
+you can roll d4s,d6s,d8s,d10s,d%s and d20s
--- main.js
+++ main.js
@@ -1,20 +1,48 @@
-const custom = document.getElementById("roll_button")
-const D4 = document.getElementById("D4")
-function d4() {
+const custom = document.getElementById("roll_button") //custom
+const D4 = document.getElementById("D4") //d4
+const D6 = document.getElementById("D6") //d6
+const D8 = document.getElementById("D8") //d8
+const D10 = document.getElementById("D10") //d10
+const Dpercent = document.getElementById("D%") //d%
+
+
+function d4() {  //d4 function
      let resultd4 = Math.floor((Math.random() * 4) + 1)
      alert(resultd4)
 
 }
 
 
+function d6() {  //d6 function
+     let resultd6 = Math.floor((Math.random() * 6) + 1)
+     alert(resultd6)
+
+}   
 
 
-function custom_roll() {
+function d8() {  //d8 function
+     let resultd8 = Math.floor((Math.random() * 8) + 1)
+     alert(resultd8)
+
+}
+
+function d10() {  //d10 function
+     let resultd10 = Math.floor((Math.random() * 10) + 1)
+     alert(resultd10)
+
+}
+
+function dper() {
+    let resultdper = Math.floor((Math.random() * 100) + 1)
+     alert(resultdper)
+}
+
+function custom_roll() { // custom sides
     let result = Math.floor((Math.random() * prompt("sides?")) + 1)
     alert(result)
 }
 
-function custom_times() {
+function custom_times() { // custom times
     let times = 0
     let times2 = prompt("times?")
     while (times < times2) {
@@ -27,6 +55,10 @@ function custom_times() {
 
 
 }
-
+ // makes the buttons button
 custom.onclick = custom_times
 D4.onclick = d4
+D6.onclick =  d6
+D8.onclick = d8
+D10.onclick = d10
+Dpercent.onclick = dper
